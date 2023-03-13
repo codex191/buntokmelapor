2c730e94a4b532a106cb33581f3b18141760ce8e
Brian Agustian K <agustianbrian25@gmail.com>
2023-03-13 21:12:00 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:buntokmelapor/app/utils/error_page.dart';
+import 'package:buntokmelapor/app/utils/loading_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -5,11 +8,33 @@ import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
-      getPages: AppPages.routes,
-    ),
-  );
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: _initialization,
+        builder: (context, snapshot) {
+          //Cek Error
+          if (snapshot.hasError) {
+            return ErrorPage();
+          }
+
+          //Jika berhasil, akan menampilkan halaman aplikasi
+          if (snapshot.connectionState == ConnectionState.done) {
+            return GetMaterialApp(
+              title: "Application",
+              initialRoute: AppPages.INITIAL,
+              getPages: AppPages.routes,
+            );
+          }
+
+          //Jika tidak berhasil, akan menampilkan
+          return LoadingPage();
+        });
+  }
 }
