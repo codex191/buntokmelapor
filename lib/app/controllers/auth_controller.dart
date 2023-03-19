7dd25bf27cb76d37f1da9124566b8279955fe594
Brian Agustian K <agustianbrian25@gmail.com>
2023-03-19 14:13:54 +0700

    Menghubungkan google sign in dan firebase auth

--- lib/app/controllers/auth_controller.dart
+++ lib/app/controllers/auth_controller.dart
@@ -1,4 +1,5 @@
 import 'package:buntokmelapor/app/routes/app_pages.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -7,24 +8,44 @@ class AuthController extends GetxController {
   var isAuth = false.obs;
   GoogleSignIn _googleSignIn = GoogleSignIn();
   GoogleSignInAccount? _currentUser;
+  UserCredential? userCredential;
 
   Future<void> login() async {
     // Get.offAllNamed(Routes.HOME);
     try {
+      //Menghandle bocornya data user sebelum login
       await _googleSignIn.signOut();
+
+      // Untuk mendapatkan user google account
       await _googleSignIn.signIn().then((value) => _currentUser = value);
-      await _googleSignIn.isSignedIn().then((value) {
-        if (value) {
-          // kondisi sudah llogin
-          print("Berhasil Login");
-          print(_currentUser);
-          isAuth.value = true;
-          Get.offAllNamed(Routes.HOME);
-        } else {
-          //kondisi tidak login
-          print("Tidak berhasil Login");
-        }
-      });
+
+      // Untuk mengecek status login user
+      final isSignin = await _googleSignIn.isSignedIn();
+
+      if (isSignin) {
+        // kondisi sudah llogin
+        print("Berhasil Login");
+        print(_currentUser);
+
+        final googleAuth = await _currentUser!.authentication;
+
+        final credential = GoogleAuthProvider.credential(
+          idToken: googleAuth.idToken,
+          accessToken: googleAuth.accessToken,
+        );
+
+        await FirebaseAuth.instance
+            .signInWithCredential(credential)
+            .then((value) => userCredential = value);
+        print("user credential");
+        print(userCredential);
+
+        isAuth.value = true;
+        Get.offAllNamed(Routes.HOME);
+      } else {
+        //kondisi tidak login
+        print("Tidak berhasil Login");
+      }
     } catch (error) {
       print(error);
     }
