ea2920289e3409b0a5ddf07cdd08a42594c2e441
Brian Agustian K <agustianbrian25@gmail.com>
2023-04-27 19:56:52 +0700

    Fix UI chat admin maupun user

--- lib/app/modules/chat/views/chat_view.dart
+++ lib/app/modules/chat/views/chat_view.dart
@@ -16,6 +16,8 @@ class ChatView extends GetView<ChatController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        leadingWidth: 100,
         leading: InkWell(
           onTap: () => Get.back(),
           borderRadius: BorderRadius.circular(100),
@@ -29,40 +31,42 @@ class ChatView extends GetView<ChatController> {
                 radius: 25,
                 backgroundColor: Colors.grey,
                 child: StreamBuilder<DocumentSnapshot<Object?>>(
-                    stream: controller.streamFriendData(
-                        (Get.arguments as Map<String, dynamic>)["friendEmail"]),
-                    builder: (context, snapFriendUser) {
-                      if (snapFriendUser.connectionState ==
-                          ConnectionState.active) {
-                        var dataFriend =
-                            snapFriendUser.data!.data() as Map<String, dynamic>;
-                        if (dataFriend["photoUrl"] == "noimage") {
-                          return ClipRRect(
-                            borderRadius: BorderRadius.circular(50),
-                            child: Image.asset(
-                              "assets/logo/LogoKominfoTanpaTeks.png",
-                              fit: BoxFit.cover,
-                            ),
-                          );
-                        } else {
-                          return ClipRRect(
-                            borderRadius: BorderRadius.circular(50),
-                            child: Image.network(
-                              dataFriend["photoUrl"],
-                              fit: BoxFit.cover,
-                            ),
-                          );
-                        }
+                  stream: controller.streamFriendData(
+                      (Get.arguments as Map<String, dynamic>)["friendEmail"]),
+                  builder: (context, snapFriendUser) {
+                    if (snapFriendUser.connectionState ==
+                        ConnectionState.active) {
+                      var dataFriend =
+                          snapFriendUser.data!.data() as Map<String, dynamic>;
+
+                      if (dataFriend["photoUrl"] == "noimage") {
+                        return ClipRRect(
+                          borderRadius: BorderRadius.circular(50),
+                          child: Image.asset(
+                            "assets/logo/noimage.png",
+                            fit: BoxFit.cover,
+                          ),
+                        );
+                      } else {
+                        return ClipRRect(
+                          borderRadius: BorderRadius.circular(50),
+                          child: Image.network(
+                            dataFriend["photoUrl"],
+                            fit: BoxFit.cover,
+                          ),
+                        );
                       }
-                      return ClipRRect(
-                        borderRadius: BorderRadius.circular(50),
-                        child: Image.asset(
-                          "assets/logo/LogoKominfoTanpaTeks.png",
-                          fit: BoxFit.cover,
-                        ),
-                      );
-                    }),
-              )
+                    }
+                    return ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Image.asset(
+                        "assets/logo/noimage.png",
+                        fit: BoxFit.cover,
+                      ),
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
@@ -87,27 +91,26 @@ class ChatView extends GetView<ChatController> {
                     dataFriend["email"],
                     style: TextStyle(
                       fontSize: 14,
-                      fontWeight: FontWeight.w600,
                     ),
                   ),
                 ],
               );
             }
+
             return Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  "Memuat Data....",
+                  'Loading...',
                   style: TextStyle(
                     fontSize: 18,
                     fontWeight: FontWeight.w600,
                   ),
                 ),
                 Text(
-                  "Memuat Data.....",
+                  'Loading...',
                   style: TextStyle(
                     fontSize: 14,
-                    fontWeight: FontWeight.w600,
                   ),
                 ),
               ],
@@ -121,22 +124,53 @@ class ChatView extends GetView<ChatController> {
           Expanded(
             child: Container(
               child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-                  stream: controller.streamChats(chat_id),
-                  builder: (context, snapshot) {
-                    Timer(Duration.zero, () {
-                      controller.scrollC
-                          .jumpTo(controller.scrollC.position.maxScrollExtent);
-                    });
-                    if (snapshot.connectionState == ConnectionState.active) {
-                      var alldata = snapshot.data!.docs;
-                      return ListView.builder(
-                        controller: controller.scrollC,
-                        itemCount: alldata.length,
-                        itemBuilder: (context, index) {
-                          if (index == 0) {
+                stream: controller.streamChats(chat_id),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.active) {
+                    var alldata = snapshot.data!.docs;
+                    Timer(
+                      Duration.zero,
+                      () => controller.scrollC
+                          .jumpTo(controller.scrollC.position.maxScrollExtent),
+                    );
+                    return ListView.builder(
+                      controller: controller.scrollC,
+                      itemCount: alldata.length,
+                      itemBuilder: (context, index) {
+                        if (index == 0) {
+                          return Column(
+                            children: [
+                              SizedBox(height: 10),
+                              Text(
+                                "${alldata[index]["groupTime"]}",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              ItemChat(
+                                msg: "${alldata[index]["msg"]}",
+                                isSender: alldata[index]["pengirim"] ==
+                                        authC.user.value.email!
+                                    ? true
+                                    : false,
+                                time: "${alldata[index]["time"]}",
+                              ),
+                            ],
+                          );
+                        } else {
+                          if (alldata[index]["groupTime"] ==
+                              alldata[index - 1]["groupTime"]) {
+                            return ItemChat(
+                              msg: "${alldata[index]["msg"]}",
+                              isSender: alldata[index]["pengirim"] ==
+                                      authC.user.value.email!
+                                  ? true
+                                  : false,
+                              time: "${alldata[index]["time"]}",
+                            );
+                          } else {
                             return Column(
                               children: [
-                                SizedBox(height: 10),
                                 Text(
                                   "${alldata[index]["groupTime"]}",
                                   style: TextStyle(
@@ -144,68 +178,28 @@ class ChatView extends GetView<ChatController> {
                                   ),
                                 ),
                                 ItemChat(
+                                  msg: "${alldata[index]["msg"]}",
                                   isSender: alldata[index]["pengirim"] ==
                                           authC.user.value.email!
                                       ? true
                                       : false,
-                                  msg: "${alldata[index]["msg"]}",
                                   time: "${alldata[index]["time"]}",
                                 ),
                               ],
                             );
-                          } else {
-                            if (alldata[index]["groupTime"] ==
-                                alldata[index - 1]["groupTime"]) {
-                              ItemChat(
-                                isSender: alldata[index]["pengirim"] ==
-                                        authC.user.value.email!
-                                    ? true
-                                    : false,
-                                msg: "${alldata[index]["msg"]}",
-                                time: "${alldata[index]["time"]}",
-                              );
-                            } else {
-                              return Column(
-                                children: [
-                                  Text("${alldata[index]["groupTime"]}"),
-                                  ItemChat(
-                                    isSender: alldata[index]["pengirim"] ==
-                                            authC.user.value.email!
-                                        ? true
-                                        : false,
-                                    msg: "${alldata[index]["msg"]}",
-                                    time: "${alldata[index]["time"]}",
-                                  ),
-                                ],
-                              );
-                            }
                           }
-                        },
-                      );
-                    }
-                    return Center(
-                      child: CircularProgressIndicator(),
+                        }
+                      },
                     );
-                  }),
-              // child: ListView(
-              //   children: [
-              //     ItemChat(
-              //       msg: "Haloo",
-              //       isSender: true,
-              //     ),
-              //     ItemChat(
-              //       msg: "Halo...",
-              //       isSender: false,
-              //     ),
-              //   ],
-              // ),
+                  }
+                  return Center(child: CircularProgressIndicator());
+                },
+              ),
             ),
           ),
           Container(
-            margin: EdgeInsets.only(bottom: context.mediaQueryPadding.bottom),
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             width: Get.width,
-            //color: Colors.amber,
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -215,35 +209,33 @@ class ChatView extends GetView<ChatController> {
                       autocorrect: false,
                       controller: controller.chatC,
                       onEditingComplete: () => controller.newChat(
-                          authC.user.value.email!,
-                          Get.arguments as Map<String, dynamic>,
-                          controller.chatC.text),
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
+                        authC.user.value.email!,
+                        Get.arguments as Map<String, dynamic>,
+                        controller.chatC.text,
                       ),
                     ),
                   ),
                 ),
-                SizedBox(
-                  width: 10,
-                ),
+                SizedBox(width: 10),
                 Material(
                   borderRadius: BorderRadius.circular(100),
                   color: Colors.blueAccent,
                   child: InkWell(
+                    borderRadius: BorderRadius.circular(100),
                     onTap: () => controller.newChat(
-                        authC.user.value.email!,
-                        Get.arguments as Map<String, dynamic>,
-                        controller.chatC.text),
+                      authC.user.value.email!,
+                      Get.arguments as Map<String, dynamic>,
+                      controller.chatC.text,
+                    ),
                     child: Padding(
-                      padding: const EdgeInsets.all(15),
+                      padding: const EdgeInsets.all(16),
                       child: Icon(
                         Icons.send,
                         color: Colors.white,
                       ),
                     ),
                   ),
-                )
+                ),
               ],
             ),
           ),
@@ -255,11 +247,11 @@ class ChatView extends GetView<ChatController> {
 
 class ItemChat extends StatelessWidget {
   const ItemChat({
-    super.key,
+    Key? key,
     required this.isSender,
     required this.msg,
     required this.time,
-  });
+  }) : super(key: key);
 
   final bool isSender;
   final String msg;
@@ -278,19 +270,19 @@ class ItemChat extends StatelessWidget {
         children: [
           Container(
             decoration: BoxDecoration(
-                color: Colors.blueAccent,
-                borderRadius: isSender
-                    ? BorderRadius.only(
-                        topLeft: Radius.circular(15),
-                        topRight: Radius.circular(15),
-                        bottomLeft: Radius.circular(15),
-                      )
-                    : BorderRadius.only(
-                        topLeft: Radius.circular(15),
-                        topRight: Radius.circular(15),
-                        bottomRight: Radius.circular(15),
-                      )),
-            //color: Colors.blueAccent,
+              color: Colors.blueAccent,
+              borderRadius: isSender
+                  ? BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                      bottomLeft: Radius.circular(15),
+                    )
+                  : BorderRadius.only(
+                      topLeft: Radius.circular(15),
+                      topRight: Radius.circular(15),
+                      bottomRight: Radius.circular(15),
+                    ),
+            ),
             padding: EdgeInsets.all(15),
             child: Text(
               "$msg",
@@ -299,9 +291,7 @@ class ItemChat extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(
-            height: 5,
-          ),
+          SizedBox(height: 5),
           Text(DateFormat.jm().format(DateTime.parse(time))),
         ],
       ),
